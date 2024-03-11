effd7f19c7863ea4bb0d1f031a48f47b718c758a
Sumerian <shemerov.s@yandex.ru>
2024-03-11 16:02:44 +0300

    get 100% test area

--- utils/arrs.py
+++ utils/arrs.py
@@ -26,18 +26,14 @@ def my_slice(coll, start=None, end=None):
     """
 
     length = len(coll)
-
     if length == 0:
         return []
-
     if start is None:
         normalized_start = 0
     else:
         normalized_start = start
-
     if end is None or end > length:
         normalized_end = length
     else:
         normalized_end = end
-
     return coll[normalized_start:normalized_end]
